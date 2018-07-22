c8961bde317a5cec9ff9638c3377a1e4dcad55d3
GJzh <guojunvictory@gmail.com>
2018-07-22 10:33:49 -0700

    c++ 377

---
+++ C++/377. Combination Sum IV.cpp
@@ -0,0 +1,18 @@
+class Solution {
+public:
+    int combinationSum4(vector<int>& nums, int target) {
+        if(!nums.size()) return 0;
+        vector<int> dp(target+1,0);
+        dp[0]=1;
+        for(int i=1;i<=target;i++)
+        {
+            for (int j=0;j<nums.size();j++)
+            {
+                if(nums[j]<=i)
+                dp[i] += dp[i-nums[j]];
+            }
+        }
+        return dp[target];
+    }
+};
+
