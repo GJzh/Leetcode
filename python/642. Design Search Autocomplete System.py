6ccca001e0afa023e20d5ba8edfb6ca4c0f8f74f
GJzh <guojunvictory@gmail.com>
2018-09-02 11:42:14 -0700

    642

--- python/642. Design Search Autocomplete System.py
+++ python/642. Design Search Autocomplete System.py
@@ -1,21 +1,19 @@
-class AutocompleteSystem:
-    class Node:
+class AutocompleteSystem(object):
+    class Node(object):
         def __init__(self):
-            self.sentences = collections.defaultdict(int)
-            self.next = collections.defaultdict(int)
+            self.sentences = {}
+            self.next = {}
     
     def __init__(self, sentences, times):
         """
         :type sentences: List[str]
         :type times: List[int]
         """
-        n = len(times)
-        self.prefix = ""
         self.root = self.Node()
-        self.node = self.root
-        for i in range(n):
+        for i in range(len(sentences)):
             self.addSentence(sentences[i], times[i])
-            
+        self.prefix = ""
+        self.cur = self.root
         
     def addSentence(self, sentence, time):
         node = self.root
@@ -23,31 +21,47 @@ class AutocompleteSystem:
             if c not in node.next:
                 node.next[c] = self.Node()
             node = node.next[c]
-            #if sentence not in node.sentences: node.sentences[sentence] = 0
+            if sentence not in node.sentences:
+                node.sentences[sentence] = 0
             node.sentences[sentence] += time
-                
-        
+            
 
     def input(self, c):
         """
         :type c: str
         :rtype: List[str]
         """
-        if c == '#':
+        if c == "#":
             self.addSentence(self.prefix, 1)
+            self.cur = self.root
             self.prefix = ""
-            self.node = self.root
             return []
         else:
             self.prefix += c
-            if c not in self.node.next:
-                self.node.next[c] = self.Node()
-            self.node = self.node.next[c]
-            res = []
-            cnt = 0
-            for sentence, time in sorted(self.node.sentences.items(), key = lambda item: (-item[1], item[0]) ):
-                res.append(sentence)
-                cnt += 1
-                if cnt >= 3: break
-            return res
+            if c not in self.cur.next:
+                self.cur.next[c] = self.Node()
+            self.cur = self.cur.next[c]
+            return self.pickTop3()
+        
+    def pickTop3(self):
+        if len(self.cur.sentences) == 0:
+            return []
+        cnt = 0
+        res = []
+        for sentence, time in sorted(self.cur.sentences.items(), key = self.findNum):
+            res.append(sentence)
+            cnt += 1
+            if cnt >= 3:
+                break
+        return res
+        
+        
+    def findNum(self, item):
+        return (-item[1], item[0])
+            
+        
 
+
+# Your AutocompleteSystem object will be instantiated and called as such:
+# obj = AutocompleteSystem(sentences, times)
+# param_1 = obj.input(c)
