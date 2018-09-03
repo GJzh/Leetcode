74df9a34fed5779554f2b2a069d85cc8c3cd886b
GJzh <guojunvictory@gmail.com>
2018-09-03 09:09:49 -0700

    165

--- python/165. Compare Version Numbers.py
+++ python/165. Compare Version Numbers.py
@@ -1,43 +1,33 @@
-class Solution:
+class Solution(object):
+    def findOneNumber(self, version, index):
+        n = len(version)
+        num = 0
+        while index < n and version[index] != '.':
+            num = 10 * num + ord(version[index]) - ord("0")
+            index += 1
+        while index < n and version[index] == '.':
+            index += 1
+        return num, index
+        
     def compareVersion(self, version1, version2):
         """
         :type version1: str
         :type version2: str
         :rtype: int
         """
-        i = 0
-        j = 0
-        while i < len(version1) and j < len(version2):
-            num1 = num2 = 0
-            while i < len(version1) and version1[i] != '.':
-                num1 = 10 * num1 + ord(version1[i]) - ord('0')
-                i += 1
-            while j < len(version2) and version2[j] != '.':
-                num2 = 10 * num2 + ord(version2[j]) - ord('0')
-                j += 1
+        m = len(version1)
+        n = len(version2)
+        i, j = 0, 0
+        while i < m and j < n:
+            num1, i = self.findOneNumber(version1, i)
+            num2, j = self.findOneNumber(version2, j)
             if num1 < num2: return -1
             elif num1 > num2: return 1
             else: pass
-            while i < len(version1) and version1[i] == '.': i += 1
-            while j < len(version2) and version2[j] == '.': j += 1
-        # process the residual tail, if the numbers in residual are 0s, we should return 0
-        if i < len(version1): 
-            num1 = 0
-            while i < len(version1):
-                while i < len(version1) and version1[i] != '.':
-                    num1 = 10 * num1 + ord(version1[i]) - ord('0')
-                    i += 1
-                if num1 > 0: return 1
-                while i < len(version1) and version1[i] == '.': i += 1
-            return 0 
-        elif j < len(version2):
-            num2 = 0
-            while j < len(version2):
-                while j < len(version2) and version2[j] != '.':
-                    num2 = 10 * num2 + ord(version2[j]) - ord('0')
-                    j += 1
-                if num2 > 0: return -1
-                while j < len(version2) and version2[j] == '.': j += 1
-            return 0
-        else: return 0
-
+        while i < m:
+            num1, i = self.findOneNumber(version1, i)
+            if num1 > 0: return 1
+        while j < n:
+            num2, j = self.findOneNumber(version2, j)
+            if num2 > 0: return -1
+        return 0
