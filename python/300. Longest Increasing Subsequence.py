48fff21429e2a6cc0cb6883e6e957c66e5ec12dc
GJzh <guojunvictory@gmail.com>
2018-10-12 15:58:26 -0700

    300

--- python/300. Longest Increasing Subsequence.py
+++ python/300. Longest Increasing Subsequence.py
@@ -1,3 +1,23 @@
+Solution 1:
+# O(n^2)
+class Solution(object):
+    def lengthOfLIS(self, nums):
+        """
+        :type nums: List[int]
+        :rtype: int
+        """
+        n = len(nums)
+        if n == 0: return 0
+        dp = [1] * n 
+        res = 1
+        for i in range(1, n):
+            for j in range(i-1, -1, -1):
+                if nums[j] < nums[i]:
+                    dp[i] = max(dp[i], dp[j]+1)
+                res = max(res, dp[i])
+        return res
+Solution 2:
+# O(nlogn)
 class Solution(object):
     def lengthOfLIS(self, nums):
         """
