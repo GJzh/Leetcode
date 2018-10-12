192a5f9dcf8944702eebb512787b91b8cfe83a64
GJzh <guojunvictory@gmail.com>
2018-10-11 17:41:26 -0700

    283

--- python/283. Move Zeroes.py
+++ python/283. Move Zeroes.py
@@ -1,3 +1,4 @@
+Solution 1:
 class Solution(object):
     def moveZeroes(self, nums):
         """
@@ -13,3 +14,16 @@ class Solution(object):
                 cnt += 1
         for i in range(cnt,n):
             nums[i] = 0
+
+Solution 2:
+class Solution(object):
+    def moveZeroes(self, nums):
+        """
+        :type nums: List[int]
+        :rtype: void Do not return anything, modify nums in-place instead.
+        """
+        idx = 0
+        for i in range(len(nums)):
+            if nums[i] != 0:
+                nums[idx], nums[i] = nums[i], nums[idx]
+                idx += 1
