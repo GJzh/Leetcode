42827631034b79a0c8d09ee1169138c8aac9ddcb
GJzh <guojunvictory@gmail.com>
2018-10-13 00:40:02 -0700

    76

--- python/076. Minimum Window Substring.py
+++ python/076. Minimum Window Substring.py
@@ -5,35 +5,38 @@ class Solution(object):
         :type t: str
         :rtype: str
         """
-        m = len(s)
-        n = len(t)
         status = {}
         for c in t:
             if c not in status:
                 status[c] = 1
             else:
                 status[c] += 1
-        cnt = n
-        left = 0
-        right = 0
-        window = ""
-        l = m+1
-        while right < m:
-            while right < m and cnt > 0:
+        cnt = len(status)
+        left, right = 0, 0
+        n = len(s)
+        start = -1
+        end = -1
+        minL = float('inf')
+        while right < n:
+            # move right
+            while right < n and cnt > 0:
                 if s[right] in status:
                     status[s[right]] -= 1
-                    if status[s[right]] >= 0: cnt -= 1
+                    if status[s[right]] == 0:
+                        cnt -= 1
                 right += 1
-            while left < right:
+            if cnt > 0: break
+            # move left
+            while left < right and cnt == 0:
                 if s[left] in status:
                     status[s[left]] += 1
                     if status[s[left]] > 0:
-                        break
+                        cnt += 1
                 left += 1
-            if cnt == 0 and (right - left) < l:
-                window = s[left:right]
-                l = len(window)
-            cnt += 1
+            left -= 1
+            # keep track of the minimum substring
+            if right - left < minL:
+                start, end = left, right
+                minL = right - left
             left += 1
-                
-        return window 
+        return s[start:end] if start >= 0 else ""
