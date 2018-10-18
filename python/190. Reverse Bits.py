21e87b7154d3b2c3db35a347a32643fc32bcbcbd
GJzh <guojunvictory@gmail.com>
2018-10-18 10:07:49 -0700

    190

---
+++ python/190. Reverse Bits.py
@@ -0,0 +1,10 @@
+class Solution:
+    # @param n, an integer
+    # @return an integer
+    def reverseBits(self, n):
+        res = 0
+        for i in range(32):
+            res <<= 1
+            res += (n & 1)
+            n >>= 1
+        return res
