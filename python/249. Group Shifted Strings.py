99333682b355eb98c027e3d2a4190da972a74362
GJzh <guojunvictory@gmail.com>
2018-10-18 21:09:54 -0700

    249

--- python/249. Group Shifted Strings.py
+++ python/249. Group Shifted Strings.py
@@ -1,3 +1,4 @@
+Solution 1
 class Solution(object):
     def getDiff(self, s):
         res = ""
@@ -21,4 +22,42 @@ class Solution(object):
         res = []
         for key, value in status.items():
             res.append(value)
+        return res
+
+Solution 2
+class Solution(object):
+    class PrefixTree():
+        class Node():
+            def __init__(self):
+                self.next = {}
+                self.words = []
+            
+        def __init__(self, strings):
+            self.root = self.Node()
+            for s in strings:
+                self.addString(s)
+            
+        def addString(self, s):
+            node = self.root
+            for i in range(1, len(s)):
+                diff = (ord(s[i]) - ord(s[i-1]) + 26) % 26
+                if diff not in node.next:
+                    node.next[diff] = self.Node()
+                node = node.next[diff]
+            node.words.append(s)
+            
+        def search(self, node, res):
+            if node == None: return
+            if len(node.words) > 0: res.append(node.words)
+            for _, nextNode in node.next.items():
+                self.search(nextNode, res)
+        
+    def groupStrings(self, strings):
+        """
+        :type strings: List[str]
+        :rtype: List[List[str]]
+        """
+        tree = self.PrefixTree(strings)
+        res = []
+        tree.search(tree.root, res)
         return res
