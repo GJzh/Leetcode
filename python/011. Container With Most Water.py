3b8a650d86b8908bd6580625c4d871c48661ff48
GJzh <guojunvictory@gmail.com>
2018-10-25 15:24:26 -0700

    11

---
+++ python/011. Container With Most Water.py
@@ -0,0 +1,19 @@
+class Solution(object):
+    def maxArea(self, height):
+        """
+        :type height: List[int]
+        :rtype: int
+        """
+        n = len(height)
+        left, right = 0, n-1
+        area = 0
+        while left < right:
+            area = max(area, min(height[left], height[right]) * (right - left))
+            if height[left] < height[right]:
+                left += 1
+            elif height[left] > height[right]:
+                right -= 1
+            else:
+                left += 1
+                right -= 1
+        return area
