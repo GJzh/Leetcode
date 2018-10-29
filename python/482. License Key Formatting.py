b0311009188e2de9190b4788caddadcadf612b2f
GJzh <guojunvictory@gmail.com>
2018-10-29 16:57:01 -0700

    482

--- python/482. License Key Formatting.py
+++ python/482. License Key Formatting.py
@@ -10,9 +10,8 @@ class Solution(object):
         if n == 0: return ""
         l = n % K
         idx = 0
-        ans = S[:l]
-        if len(ans) > 0: ans += "-"
+        ans = []
+        if l > 0: ans.append(S[:l])
         for i in range(1, n/K+1):
-            ans += S[l+(i-1)*K:l+i*K]
-            ans += "-"
-        return ans[:len(ans)-1]
+            ans.append(S[l+(i-1)*K:l+i*K])
+        return "-".join(ans)
