0ebdffc3130b94e91dfbbe5e8edfd323c87d5860
GJzh <guojunvictory@gmail.com>
2018-10-30 22:20:46 -0700

    227

--- python/227. Basic Calculator II.py
+++ python/227. Basic Calculator II.py
@@ -1,3 +1,4 @@
+Solution 1
 class Solution(object):
     def calculate(self, s):
         """
@@ -47,3 +48,53 @@ class Solution(object):
             else: return 0
 
         return part1 + part2
+
+Solution 2:
+class Solution(object):
+    def _calculate(self, operands, operators):
+        val1 = operands[-1]
+        val2 = operands[-2]
+        operands.pop()
+        operands.pop()
+        operator = operators[-1]
+        operators.pop()
+        if operator == "+":
+            val = val1 + val2
+        elif operator == "-":
+            val = val1 - val2
+        elif operator == "*":    
+            val = val1 * val2
+        elif operator == "/":    
+            val = val1 / val2
+        else:
+            print("We do not support the operator " + operator)
+        operands.append(val)
+        return
+        
+    def calculate(self, s):
+        """
+        :type s: str
+        :rtype: int
+        """
+        n = len(s)
+        if n == 0: return 0
+        cur = ""
+        operands = []
+        operators = []
+        for i in range(n-1, -1, -1):
+            if s[i].isdigit():
+                cur += s[i]
+                if i == 0 or not s[i-1].isdigit():
+                    operands.append(int(cur[::-1]))
+                    cur = ""
+            elif s[i] == "+" or s[i] == "-":
+                while len(operators) and operators[-1] != "+" and operators[-1] != "-":
+                    self._calculate(operands, operators)
+                operators.append(s[i])
+            elif s[i] == "*" or s[i] == "/":
+                operators.append(s[i])
+            else:
+                pass
+        while len(operators):
+            self._calculate(operands, operators)
+        return operands[0]
--- python/282. Expression Add Operators.py
+++ python/282. Expression Add Operators.py
@@ -1,29 +1,17 @@
 class Solution(object):
-    def dfs(self, num, target, idx, part1, part2, cur, res):
-        if idx == len(num):
-            if part1 + part2 == target:
-                res.append(cur)
-            return
-        m = len(cur)
+    def dfs(self, num, target, idx, val1, val2, expression, ans):
+        if idx >= len(num):
+            if val1 + val2 == target:
+                ans.append(expression)
         for i in range(idx, len(num)):
-            val = int(num[idx:i+1])
-            # "+"
-            cur += ('+' + num[idx:i+1])
-            self.dfs(num, target, i+1, part1+part2, val, cur, res)
-            cur = cur[:m]
-            # "-"
-            cur += ('-' + num[idx:i+1])
-            self.dfs(num, target, i+1, part1+part2, -val, cur, res)
-            cur = cur[:m]
-            # "*"
-            cur += ('*' + num[idx:i+1])
-            self.dfs(num, target, i+1, part1, part2 * val, cur, res)
-            cur = cur[:m]
-            if val == 0:
-                break
+            cur = num[idx:i+1]
+            val = int(cur)
+            self.dfs(num, target, i+1, val1+val2, val, expression + "+" + cur, ans)
+            self.dfs(num, target, i+1, val1+val2, -val, expression + "-" + cur, ans)
+            self.dfs(num, target, i+1, val1, val2 * val, expression + "*" + cur, ans)
+            if val == 0: break
         return
-            
-    
+        
     def addOperators(self, num, target):
         """
         :type num: str
@@ -32,11 +20,11 @@ class Solution(object):
         """
         n = len(num)
         if n == 0: return []
-        res = []
+        val1 = 0
+        ans = []
         for i in range(n):
-            val = int(num[:i+1])
-            cur = num[:i+1]
-            self.dfs(num, target, i+1, 0, val, cur, res)
-            if val == 0:
-                break
-        return res
+            expression = num[:i+1]
+            val2 = int(expression)
+            self.dfs(num, target, i+1, val1, val2, expression, ans)
+            if val2 == 0: break
+        return ans
