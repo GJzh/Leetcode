c571327173081ebb8579722bfb52c11fc5bd0d7d
GJzh <guojunvictory@gmail.com>
2018-10-30 21:29:25 -0700

    50

--- python/050. Pow(x, n).py
+++ python/050. Pow(x, n).py
@@ -1,14 +1,9 @@
 class Solution(object):
     def _myPow(self, x, n):
-        if n == 0:
-            return 1
-        if n == 1:
-            return x
-        res = x if n & 1 else 1
-        temp = self._myPow(x, n/2)
-        res *= (temp * temp)
-        return res
-        
+        if n == 1: return x
+        val1 = self._myPow(x, n/2)
+        val2 = x if n & 1 else 1
+        return val1 * val1 * val2
     
     def myPow(self, x, n):
         """
@@ -16,14 +11,7 @@ class Solution(object):
         :type n: int
         :rtype: float
         """
-        if n == 0:
-            return 1
-        if x == 0:
-            return 0
-        if n < 0:
-            sign = -1
-        else:
-            sign = 1
-        n = abs(n)
-        res = self._myPow(x, n)
-        return res if sign == 1 else 1.0 / res
+        if n == 0: return 1
+        if x == 0: return 0
+        if n < 0: x = 1.0 / x
+        return self._myPow(x, abs(n))
