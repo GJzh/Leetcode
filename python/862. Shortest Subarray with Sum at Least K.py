5f8edaf7836524ee7812f3e2868f0b8ab63b8c8e
GJzh <guojunvictory@gmail.com>
2018-10-31 22:52:45 -0700

    862

--- python/862. Shortest Subarray with Sum at Least K.py
+++ python/862. Shortest Subarray with Sum at Least K.py
@@ -1,4 +1,4 @@
-import queue
+from queue import deque
 class Solution:
     def shortestSubarray(self, A, K):
         """
@@ -12,7 +12,7 @@ class Solution:
         res = n+1
         for i in range(1, n+1):
             sums[i] = sums[i-1] + A[i-1]
-        dq = queue.deque([0])
+        dq = deque([0])
         for end in range(1, n+1):
             while dq and sums[end] - sums[dq[0]] >= K:
                 res = min(res, end - dq[0])
