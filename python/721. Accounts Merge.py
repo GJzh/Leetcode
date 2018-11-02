4b43d92bd63170995a5f84f14d4c2c0119c856ba
GJzh <guojunvictory@gmail.com>
2018-11-02 11:43:00 -0700

    721

--- python/721. Accounts Merge.py
+++ python/721. Accounts Merge.py
@@ -1,27 +1,27 @@
 class Solution(object):
-    class Person():
+    class UnionFind():
         def __init__(self, n):
-            self.persons = [i for i in range(n)]
-            self.jumps = [0] * n
+            self.users = [k for k in range(n)]
+            self.sz = [1] * n
         
         def find(self, k):
-            cnt = 0
-            while self.persons[k] != k:
-                k = self.persons[k]
-                cnt += 1
-            self.jumps[k] = max(self.jumps[k], cnt)
+            while self.users[k] != k:
+                self.users[k] = self.users[self.users[k]]
+                k = self.users[k]
             return k
         
-        def merge(self, p1, p2):
-            k1 = self.find(p1)
-            k2 = self.find(p2)
-            if self.jumps[k1] <= self.jumps[k2]:
-                self.persons[k1] = k2
+        def merge(self, k1, k2):
+            k1 = self.find(k1)
+            k2 = self.find(k2)
+            if k1 == k2: return k1
+            if self.sz[k1] < self.sz[k2]:
+                self.users[k1] = k2
+                self.sz[k2] += self.sz[k1]
                 return k2
             else:
-                self.persons[k2] = k1
+                self.users[k2] = k1
+                self.sz[k1] += self.sz[k2]
                 return k1
-                
     
     def accountsMerge(self, accounts):
         """
@@ -29,11 +29,9 @@ class Solution(object):
         :rtype: List[List[str]]
         """
         n = len(accounts)
-        if n == 0: return []
-        p = self.Person(n)
         emails = {}
+        p = self.UnionFind(n)
         for i in range(n):
-            name = accounts[i][0]
             for j in range(1, len(accounts[i])):
                 email = accounts[i][j]
                 if email not in emails:
@@ -41,14 +39,13 @@ class Solution(object):
                 else:
                     emails[email] = p.merge(i, emails[email])
         persons = {}
-        for email, idx in emails.items():
-            ownerId = p.find(idx)
-            if ownerId not in persons:
-                persons[ownerId] = set()
-            persons[ownerId].add(email)
-        res = []
-        for ownerId, emailList in persons.items():
-            cur = [accounts[ownerId][0]]
-            cur += sorted(list(emailList))
-            res.append(cur)
-        return res
+        for email, accoundId in emails.items():
+            personID = p.find(accoundId)
+            if personID not in persons:
+                persons[personID] = set()
+            persons[personID].add(email)
+        ans = []
+        for personID, emailSet in persons.items():
+            personName = accounts[personID][0]
+            ans.append([personName] + sorted(list(emailSet)))
+        return ans
