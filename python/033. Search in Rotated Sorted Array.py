544161319aa81299525331a2c8053512305334fe
GJzh <guojunvictory@gmail.com>
2018-11-02 14:32:10 -0700

    33

--- python/033. Search in Rotated Sorted Array.py
+++ python/033. Search in Rotated Sorted Array.py
@@ -6,29 +6,21 @@ class Solution(object):
         :rtype: int
         """
         n = len(nums)
-        if n == 0: return -1
-        self.target = target
-        self.nums = nums
-        return self._search(0, n-1)
-    
-    def _search(self, left, right):
-        if left == right: 
-            return left if self.nums[left] == self.target else -1
-        mid = (left + right) // 2
-        nums = self.nums
-        target = self.target
-        if nums[mid] == target:
-            return mid
-        if nums[mid] > nums[left]:
-            if target >= nums[left] and target < nums[mid]:
-                return self._search(left,mid-1)
+        left, right = 0, n-1
+        while left <= right:
+            mid = (left + right) / 2
+            if nums[mid] == target: return mid
+            if nums[mid] > nums[left]:
+                if nums[left] <= target and target < nums[mid]:
+                    right = mid - 1
+                else:
+                    left = mid + 1
+            elif nums[mid] < nums[left]:
+                if nums[mid] < target and target <= nums[right]:
+                    left = mid + 1
+                else:
+                    right = mid - 1
             else:
-                return self._search(mid+1,right)
-        elif nums[mid] < nums[left]:
-            if target > nums[mid] and target <= nums[right]:
-                return self._search(mid+1,right)
-            else:
-                return self._search(left,mid-1)
-        else:
-            return right if self.nums[right] == self.target else -1
+                left += 1
+        return -1
 
