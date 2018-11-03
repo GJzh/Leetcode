db4485e252f2721bcebc1afa7527f4af67ea3679
GJzh <guojunvictory@gmail.com>
2018-11-02 20:27:36 -0700

    273

--- python/273. Integer to English Words.py
+++ python/273. Integer to English Words.py
@@ -1,47 +1,31 @@
-class Solution:
+class Solution(object):
     def numberToWords(self, num):
         """
         :type num: int
         :rtype: str
         """
         if num == 0: return "Zero"
-        diction = ["Billion", "Million", "Thousand", ""]
-        digits_10 = ["", "", "Twenty", "Thirty", "Forty", "Fifty",  "Sixty", "Seventy", "Eighty", "Ninety"]
-        digits_1 = ["Ten", "Eleven", "Twelve", "Thirteen", "Fourteen", "Fifteen",
-                    "Sixteen", "Seventeen", "Eighteen", "Nineteen"]
-        digits = ["Zero", "One", "Two", "Three", "Four", "Five", "Six", "Seven", "Eight", "Nine"]
-        res = ""
-        while num > 0: 
-            for i in range(4):
-                base = 10 ** (9 - 3 * i)
-                val = num // base
-                if val > 0:
-                    hundreds = val // 100
-                    residual = val % 100
-                    val_10 = residual // 10
-                    val_1 = residual % 10 if residual >= 10 and residual < 20 else -1
-                    val_0 = residual % 10
-                    if hundreds > 0:
-                        res += digits[hundreds]
-                        res += " Hundred "
-                    if val_1 >= 0:
-                        res += digits_1[val_1]
-                        res += " "
-                    else:
-                        if val_10:
-                            res += digits_10[val_10]
-                            res += " "
-                        if val_0:
-                            res += digits[val_0]
-                            res += " "
-                    
-                    res += diction[i]
-                    res += " "
-                    
-                num %= base
-            
-        i = len(res)-1
-        while i >= 0 and res[i] == " ":
-            i -= 1
-        return res[:i+1]
-
+        digit_1 = ["", "One", "Two", "Three", "Four", "Five", "Six", "Seven", "Eight", "Nine"]
+        digit_11 = ["Ten", "Eleven", "Twelve", "Thirteen", "Fourteen", "Fifteen", "Sixteen", "Seventeen", "Eighteen", "Nineteen"]
+        digit_2 = ["", "", "Twenty", "Thirty", "Forty", "Fifty", "Sixty", "Seventy", "Eighty", "Ninety"]
+        numbers = ["", "Thousand", "Million", "Billion"]
+        ans = []
+        for i in range(3, -1, -1):
+            base = 10 ** (3 * i)
+            cur = num / base
+            num %= base
+            if cur == 0: continue
+            if cur >= 100:
+                ans.append(digit_1[cur/100])
+                ans.append("Hundred")
+            cur %= 100
+            if cur >= 20:
+                ans.append(digit_2[cur/10])
+                cur %= 10
+                if cur > 0: ans.append(digit_1[cur])
+            elif cur >= 10:
+                ans.append(digit_11[cur%10])
+            elif cur > 0:
+                ans.append(digit_1[cur])
+            if i > 0: ans.append(numbers[i])
+        return " ".join(ans)
