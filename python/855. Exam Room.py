5cd127da4878fa311ad351db1bd69960dc4264ed
GJzh <guojunvictory@gmail.com>
2018-11-17 09:22:38 -0800

    855

---
+++ python/855. Exam Room.py
@@ -0,0 +1,62 @@
+priority queue, time: O(logN), space: O(N)
+class ExamRoom(object):
+
+    def __init__(self, N):
+        """
+        :type N: int
+        """
+        self.students = {}
+        self.intervals = []
+        heapq.heappush(self.intervals, (-(N/2), 0, -1, N))
+        self.students[-1] = [-2,N]
+        self.students[N] = [-1, N+1]
+        self.N = N
+
+    def seat(self):
+        """
+        :rtype: int
+        """
+        while len(self.intervals):
+            negDist, pos, start, end = self.intervals[0]
+            if start in self.students and end in self.students and self.students[start][1] == end:
+                break
+            else:
+                heapq.heappop(self.intervals)
+        negDist, pos, start, end = self.intervals[0]
+        heapq.heappop(self.intervals)
+        if start == -1:
+            pos = 0
+            if end == self.N: dist = end-pos-1
+            else: dist = (end-pos)/2
+            heapq.heappush(self.intervals, (-dist,  (end+pos) / 2, pos, end))
+        elif end == self.N:
+            pos = self.N-1
+            heapq.heappush(self.intervals, (-((pos-start)/2), (pos+start) / 2, start, pos))
+        else:
+            pos = (end + start) / 2
+            heapq.heappush(self.intervals, (-((pos-start)/2), (pos+start) / 2, start, pos))
+            heapq.heappush(self.intervals, (-((end-pos)/2), (end+pos) / 2, pos, end))
+        self.students[pos] = [start, end]
+        self.students[start][1] = pos
+        self.students[end][0] = pos
+        return pos
+
+    def leave(self, p):
+        """
+        :type p: int
+        :rtype: void
+        """
+        a, b = self.students[p]
+        del self.students[p]
+        self.students[a][1] = b
+        self.students[b][0] = a
+        if a == -1 or b == self.N: dist = b-a-1
+        else: dist = (b-a)/2
+        heapq.heappush(self.intervals, (-dist, (a+b)/2, a, b))
+            
+
+
+# Your ExamRoom object will be instantiated and called as such:
+# obj = ExamRoom(N)
+# param_1 = obj.seat()
+# obj.leave(p)
