0e98feb15884faf4801a28b14a8489c3deb26614
GJzh <guojunvictory@gmail.com>
2018-12-02 14:22:28 -0800

    855

--- python/855. Exam Room.py
+++ python/855. Exam Room.py
@@ -6,8 +6,9 @@ class ExamRoom(object):
         :type N: int
         """
         self.students = {}
+        self.seats = {}
         self.intervals = []
-        heapq.heappush(self.intervals, (-(N/2), 0, -1, N))
+        heapq.heappush(self.intervals, (-(N-1), -1, N))
         self.students[-1] = [-2,N]
         self.students[N] = [-1, N+1]
         self.N = N
@@ -17,25 +18,25 @@ class ExamRoom(object):
         :rtype: int
         """
         while len(self.intervals):
-            negDist, pos, start, end = self.intervals[0]
+            negDist, start, end = self.intervals[0]
             if start in self.students and end in self.students and self.students[start][1] == end:
                 break
             else:
                 heapq.heappop(self.intervals)
-        negDist, pos, start, end = self.intervals[0]
+        negDist, start, end = self.intervals[0]
         heapq.heappop(self.intervals)
         if start == -1:
             pos = 0
             if end == self.N: dist = end-pos-1
             else: dist = (end-pos)/2
-            heapq.heappush(self.intervals, (-dist,  (end+pos) / 2, pos, end))
+            heapq.heappush(self.intervals, (-dist, pos, end))
         elif end == self.N:
             pos = self.N-1
-            heapq.heappush(self.intervals, (-((pos-start)/2), (pos+start) / 2, start, pos))
+            heapq.heappush(self.intervals, (-((pos-start)/2), start, pos))
         else:
             pos = (end + start) / 2
-            heapq.heappush(self.intervals, (-((pos-start)/2), (pos+start) / 2, start, pos))
-            heapq.heappush(self.intervals, (-((end-pos)/2), (end+pos) / 2, pos, end))
+            heapq.heappush(self.intervals, (-((pos-start)/2), start, pos))
+            heapq.heappush(self.intervals, (-((end-pos)/2), pos, end))
         self.students[pos] = [start, end]
         self.students[start][1] = pos
         self.students[end][0] = pos
@@ -52,7 +53,7 @@ class ExamRoom(object):
         self.students[b][0] = a
         if a == -1 or b == self.N: dist = b-a-1
         else: dist = (b-a)/2
-        heapq.heappush(self.intervals, (-dist, (a+b)/2, a, b))
+        heapq.heappush(self.intervals, (-dist, a, b))
             
 
 
